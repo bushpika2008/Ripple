dc61e9a3619e452fc27af5f8912de4df47866954
bushpika2008 <bushpika2008@gmail.com>
2025-12-16 15:36:07 +0530

    Add files via upload

---
+++ Ripple.v
@@ -0,0 +1,14 @@
+module Ripple(
+    input  wire clk,
+    input  wire reset,
+    output reg [3:0] q
+);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            q <= 4'b0000;
+        else
+            q <= q + 1'b1;   
+    end
+
+endmodule
